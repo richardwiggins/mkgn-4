4d685b4330df93e30e40c95d295ca46f4e7353e6
Richard Wiggins <richard@pixelcreation.co.uk>
2020-08-26 17:04:33 +0100

    🤷‍♂️

--- craft/web/assets/js/components/_1_responsive-sidebar.js
+++ craft/web/assets/js/components/_1_responsive-sidebar.js
@@ -13,9 +13,18 @@
     this.customStaticClass = "";
     this.readyClass = "sidebar--loaded";
     this.layout = false; // this will be static or mobile
+    this.close = document.querySelectorAll('[aria-controls="closeSidebar"]');
     getCustomStaticClass(this); // custom classes for static version
     initSidebar(this);
   };
+
+  var p = document.getElementById("closeSidebar"); // Find the paragraph element in the page
+  p.onclick = showAlert; // Add onclick function to element
+    
+  function showAlert(event) {
+    closeSidebar(sidebar);
+    alert("onclick Event triggered!");
+  }
 
   function getCustomStaticClass(element) {
     var customClasses = element.element.getAttribute('data-static-class');
--- craft/web/assets/js/scripts.js
+++ craft/web/assets/js/scripts.js
@@ -721,9 +721,18 @@ Math.easeInOutQuad = function (t, b, c, d) {
     this.customStaticClass = "";
     this.readyClass = "sidebar--loaded";
     this.layout = false; // this will be static or mobile
+    this.close = document.querySelectorAll('[aria-controls="closeSidebar"]');
     getCustomStaticClass(this); // custom classes for static version
     initSidebar(this);
   };
+
+  var p = document.getElementById("closeSidebar"); // Find the paragraph element in the page
+  p.onclick = showAlert; // Add onclick function to element
+    
+  function showAlert(event) {
+    closeSidebar(sidebar);
+    alert("onclick Event triggered!");
+  }
 
   function getCustomStaticClass(element) {
     var customClasses = element.element.getAttribute('data-static-class');
